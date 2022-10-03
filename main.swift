3cd27e13c223706a0bbaa16abe3815c5266632cf
Zachary Allen <zachary.s.allen@student.allenisd.org>
2022-10-03 13:13:33 -0500

    first version

---
+++ main.swift
@@ -0,0 +1,54 @@
+
+// Add your code below:
+var words : [String] = []
+
+var line : String?
+repeat {
+    print("Please enter a word")
+    line = readLine()
+    if line != nil {
+        words.append(line!)
+    }
+} while line != nil
+
+var array = words
+var pass = 0
+var swaps = 0
+var swapsThisPass = 0
+
+func swap(integers: inout [String], i:Int, j:Int) {
+    precondition(i < integers.count, "i with value \(i) is out of bounds")
+    precondition(j < integers.count, "j with value \(j) is out of bounds")
+    let temp = integers[j]
+    integers[j] = integers[i]
+    integers[i] = temp
+}
+
+
+
+
+
+for i in 0 ..< array.count {
+    
+    swapsThisPass = 0
+    
+    var j = i 
+
+    while j > 0 && array[j] < array[j-1] {
+        swap(integers: &array, i:j, j:j - 1)
+        swaps += 1
+        swapsThisPass += 1
+        j -= 1
+    }
+    
+    print("Pass: \(pass), Swaps: \(swapsThisPass)/\(swaps), Array: \(array)")    
+    pass += 1
+
+}
+
+    
+
+        
+    
+ 
+    
